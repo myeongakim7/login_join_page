50b1edf35632bcd2a35e572a9a110738260f19d1
Myeong A Kim <113976428+myeongakim7@users.noreply.github.com>
2023-10-25 14:26:14 +0900

    faq) 하나의 질문 클릭 시 여러 질문+답변 나오는 오류 수정

--- faq/index.js
+++ faq/index.js
@@ -7,13 +7,22 @@ $(function(){
     })
 
     // Q 클릭 시 A_BOX 열림 
-    $('.faq_list .list li .qa_box').click(function(e,num){
-        e.preventDefault();
-        $('.faq_list .list li .qa_box .a_box'+num).addClass('active');
-        // $('.faq_list .list ul li .q_box a').css('color','var(--main-color');
-        $('.faq_list .list .a_box').slideToggle();
+    // $('.faq_list .list li .qa_box').click(function(e,num){
+    //     e.preventDefault();
+    //     $('.faq_list .list li .qa_box .a_box'+num).addClass('active');
+    //     // $('.faq_list .list ul li .q_box a').css('color','var(--main-color');
+        
+    //     $('.faq_list .list .a_box').slideToggle();
     
-    })
+    // })
+
+    $('.faq_list .list li .qa_box .q_box').click(function(e){
+         e.preventDefault();
+         $('.faq_list .list li .qa_box .q_box a').css({'color':'#000'}); 
+         $('.faq_list .list li .qa_box .q_box').siblings('.a_box').slideUp();
+         $(this).siblings('.a_box').slideToggle();
+         $(this).children('a').css({'color':'var(--main-color)'});
+ })
 
 })
 
